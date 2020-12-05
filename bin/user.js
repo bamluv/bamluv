022c5d629d8981058837198f479cdede21a7c5f7
Matthew Teague <x@devmachinist.com>
2020-12-05 07:45:16 -0600

    Added npm module readline

---
+++ bin/user.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const os=require('os');
+var user = {
+	'firsName' : " ",
+	'lastName' : " ",
+	'email' : " ",
+	'password' : " ",
+	'OrgUrl' : " ",
+	'token' : " "
+};
+console.log("What's your first name?")
+console.log(Argv);
+firstname = Argv;
+console.log('You are running on a ' + os.platform());
